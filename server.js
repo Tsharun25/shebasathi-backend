ab81b5813feb8f6cec80f41cf45e586570594c36
Tsharun <harun.welltouch@gmail.com>
2026-04-13 20:06:48 +0600

    corse problem fixed

--- server.js
+++ server.js
@@ -11,15 +11,36 @@ const app = express();
 // ================= MIDDLEWARE =================
 app.use(express.json());
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://shebasathi-next.vercel.app",
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://your-frontend.vercel.app"
-    ],
+    origin: function (origin, callback) {
+      // allow requests with no origin (mobile apps, postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3000",
+//       "https://your-frontend.vercel.app"
+//     ],
+//     credentials: true,
+//   })
+// );
 
 // ================= MONGODB CONNECT =================
 mongoose
